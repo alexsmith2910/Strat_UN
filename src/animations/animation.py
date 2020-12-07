ff73e77a7ae16ceb9434ff8dc59b3122ff39ccab
SSteers126 <sebastian.steers@live.com>
2020-12-07 13:45:20 +0000

    Update to add natural HQ spawning
    HQs can now be spawned on either side of the map for player 1 and 2, avoiding water.

--- src/animations/animation.py
+++ src/animations/animation.py
@@ -8,9 +8,4 @@ for count, i in enumerate(drill_frames):
     drill_frames[count] = pyglet.image.load(drill_path + i)
     drill_frames[count].anchor_x = 10
     drill_frames[count].anchor_y = 10
-<<<<<<< HEAD:src/animations/animation.py
 #drill_ani = pyglet.image.Animation.from_image_sequence(images, duration=1.5, loop=True)
-=======
-#drill_ani = pyglet.image.Animation.from_image_sequence(images, duration=1.5, loop=True)
-sex = "me_having"
->>>>>>> 517780568de69306dda41f6ebbddcdc154acaa4f:animations/animations.py
