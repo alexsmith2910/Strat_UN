fbf0a31c6f7a544a270e954fd96ae6791a8971df
SSteers126 <sebastian.steers@live.com>
2021-01-04 16:04:39 +0000

    Change to path system
    Changed to pathlib to allow the relative paths used for animations to natively change per operating system.

--- src/animations/animation.py
+++ src/animations/animation.py
@@ -1,11 +1,28 @@
-import pyglet
+import sys
 import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-drill_path = dir_path + "\\drill\\"
+from pathlib import Path
+
+import pyglet
+
+# dir_path = os.path.dirname(os.path.realpath(__file__)) # deprecated in favour of pathlib
+
+dir_path = Path(sys.path[0])  # More consistent relative path
+
+
+drill_path = dir_path / "src" / "animations" / "drill"
+DUA_path = dir_path / "src" / "animations" / "DUA"
 drill_frames = os.listdir(drill_path)
+DUA_frames = os.listdir(DUA_path)
 
 for count, i in enumerate(drill_frames):
     drill_frames[count] = pyglet.image.load(drill_path + i)
     drill_frames[count].anchor_x = 10
     drill_frames[count].anchor_y = 10
-#drill_ani = pyglet.image.Animation.from_image_sequence(images, duration=1.5, loop=True)
+
+for count, i in enumerate(DUA_frames):
+    DUA_frames[count] = pyglet.image.load(DUA_path + i)
+    DUA_frames[count].anchor_x = 10
+    DUA_frames[count].anchor_y = 10
+
+drill_ani = pyglet.image.Animation.from_image_sequence(drill_frames, duration=0.017, loop=True)
+DUA_ani = pyglet.image.Animation.from_image_sequence(DUA_frames, duration=0.017, loop=True)
